daf355d6ac721312b97952226e5896ef21cd95bc
Justin <200430020@student.georgianc.on.ca>
2024-10-18 14:23:13 -0400

    still errors

--- ASSIGNMENT01/routes/contact.js
+++ ASSIGNMENT01/routes/contact.js
@@ -1,15 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  try {
-    res.render("contact", {
-      title: "Contact Me",
-      message: "Get in touch with me",
-    });
-  } catch (error) {
-    next(error);
-  }
+router.get("/", (req, res) => {
+  res.render("contact", {
+    title: "Contact Me",
+    message: "Get in touch with me",
+  });
 });
 
 module.exports = router;
